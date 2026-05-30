c247bce1d62ae94cea9a852988147f3b0cc6e26a
7qyr6kfprwan <7qyr6kfprwan@github.com>
2026-05-30 21:02:52 +0800

    Add total investment summary row above breakdown

--- js/app.js
+++ js/app.js
@@ -1120,6 +1120,7 @@
                 var cls = key + (isWinner ? ' winner' : '');
                 return '<div class="bt-summary-card ' + cls + '">' +
                     '<div class="bt-summary-title">[ ' + r.name + ' ] ' + r.stocks.map(function (s) { return s.symbol; }).join(' + ') + (isWinner ? ' <span class="bt-winner-badge">WINNER</span>' : '') + '</div>' +
+                    '<div class="bt-summary-row total"><span class="label">总投入</span><span class="value">' + fmtMoney(c.totalInvested) + '</span></div>' +
                     '<div class="bt-summary-row"><span class="label">定投总投入</span><span class="value">' + fmtMoney(c.cashInvested.dca) + '</span></div>' +
                     '<div class="bt-summary-row"><span class="label">RSI加仓投入</span><span class="value">' + fmtMoney(c.cashInvested.rsi) + '</span></div>' +
                     '<div class="bt-summary-row"><span class="label">RSI卖/买回</span><span class="value">' + fmtMoney(c.cashInvested.rsi_sell_rebuy) + '</span></div>' +
